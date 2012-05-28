6018a0c1bbe0da6189c50bbf3c5af5c444bcf1d0
Fernando Zunino <fernando@zauberlabs.com>
2012-05-28 13:25:40 -0300

    Inicializacion de proyecto rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zviz::Application.config.secret_token = '69ff0bad1372027c19e039b7c5368af42a7b326e03824edc2694033f6d30cf6dec4f52413b41a87c4585d1c92a0f90c0c04e395dd2c04c19e08f9d6eb30f029a'
