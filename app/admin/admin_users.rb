15e7c1b1c876ecf558c9e47c0602f7b4974f2b95
Santiago Villa Fernandez <svillafernandez@zauberlabs.com>
2012-08-03 18:01:08 -0300

    Added the new admin setup

---
+++ app/admin/admin_users.rb
@@ -0,0 +1,13 @@
+ActiveAdmin.register AdminUser do
+  index do
+    column :email
+    column :current_sign_in_at
+    column :last_sign_in_at
+    column :sign_in_count
+    default_actions
+  end
+  form do |f|
+     f.inputs :email
+    f.buttons
+ end
+end
